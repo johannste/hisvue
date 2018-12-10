6005bcd691bda35b41fada25198f3d6e428cf481
johannste <saint.sola@yahoo.com>
2018-12-10 15:23:14 +0800

    TODO

--- src/components/cash_system/register/register.vue
+++ src/components/cash_system/register/register.vue
@@ -1,51 +1,46 @@
 <template>
   <div class="register">
     <div style="text-align: center;font-size: 24px;
-    margin-bottom: 25px;" class="register-title">挂 号 办 理</div>
+    margin-bottom: 25px;" class="register-title">挂 号 办 理
+    </div>
     <el-form :model="register" :rules="rules" ref="registerForm" label-width="100px" class="register-ruleForm">
       <el-form-item label="患者姓名" prop="patientName" class="register-name">
-        <el-input disabled v-model="register.patientName"></el-input>
+        <el-input disabled v-model="register.patientName" placeholder="请输入身份信息"></el-input>
       </el-form-item>
       <el-form-item label="办理凭证" prop="certificate_value" class="register-certificate">
-        <el-select v-model="register.certificate_value" placeholder="请选择办理凭证">
-          <el-option label="身份证" value="idCard"></el-option>
-          <el-option label="健康证" value="healthCard"></el-option>
+        <el-select v-model="certificateValue" placeholder="请选择办理凭证">
+          <el-option v-for="items in identifyType" :key="items.type" :label="items.type" :value="items.id">
+          </el-option>
         </el-select>
       </el-form-item>
-      <p class="register-isshow" v-if="register.certificate_value === 'idCard'">
-        <el-form-item label="身份证号" prop="cardNumber" class="register-name">
+      <p class="register-isShow" v-if="register.certificate_value !== ''">
+        <el-form-item label="证件号" prop="cardNumber" class="register-name">
           <el-input v-model="register.cardNumber"></el-input>
         </el-form-item>
       </p>
-      <p class="register-isshow" v-else-if="register.certificate_value === 'healthCard'">
-        <el-form-item label="健康证号" prop="cardNumber" class="register-name">
-          <el-input v-model="register.cardNumber"></el-input>
-        </el-form-item>
-      </p>
-      <p v-else></p>
       <el-form-item label="科室" prop="department_value">
-        <el-select v-model="departmentvalue" placeholder="请选择科室">
-          <el-option v-for="items in department" :key="items.deptEngName" :label="items.deptName" :value="items.id">
+        <el-select v-model="departmentValue" placeholder="请选择科室">
+          <el-option v-for="items in department" :key="items.id" :label="items.deptName" :value="items.id">
           </el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="就诊医生" prop="doctor_value" class="register-certificate">
-        <el-select v-model="doctorvalue" placeholder="请选择就诊医生">
+        <el-select v-model="doctorValue" placeholder="请选择就诊医生">
           <el-option v-for="items in doctor" :key="items.id" :label="items.name" :value="items.name">
           </el-option>
         </el-select>
       </el-form-item>
-      <p v-if="doctorvalue">
+      <p v-if="doctorValue">
         <el-form-item label="就诊时间：" class="register-name">
           <span>{{ register.visit_date }}  {{ register.visit_time }}</span>
         </el-form-item>
       </p>
-      <p v-if="doctorvalue">
+      <p v-if="doctorValue">
         <el-form-item label="候诊号：" class="register-name">
           <span>{{ register.register_number }}</span>
         </el-form-item>
       </p>
-      <p v-if="doctorvalue">
+      <p v-if="doctorValue">
         <el-form-item label="挂号费：" class="register-name">
           <span>{{ register.expense }}元</span>
         </el-form-item>
@@ -55,6 +50,15 @@
         <el-button type="reset" @click="resetForm('registerForm')">重置</el-button>
       </div>
     </el-form>
+    <el-dialog title="未能识别该身份" :visible.sync="dialogVisible">
+      <h4>
+        是否要跳转到注册页面
+      </h4>
+      <h5 slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">否</el-button>
+        <el-button type="primary" @click="toRegistry()">是</el-button>
+      </h5>
+    </el-dialog>
   </div>
 </template>
 
@@ -62,27 +66,28 @@
   export default {
     data () {
       let checkCertificate = (rule, value, callback) => {
-        if (/^[1-9]\d{5}(18|19|([23]\d))\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$|^[1-9]\d{5}\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{2}$/.test(value) === false) {
-          callback(new Error('身份证号有误'));
-        } else {
-          this.$http.get('http://localhost:8081/patient/queryIndentify?identifyNumber=' + this.register.cardNumber).then((response) => {
-            if (response.bodyText !== '') {
-              this.register.patientName = response.bodyText;
-            } else {
-              this.$notify.error({
-                message: '即将跳转到注册页面'
-              });
-              setTimeout(() => {
-                this.$router.push({path: 'addPatient'});
-              }, 1000);
-            };
-          }, response => {
-            this.$notify.error({
-              message: '数据请求失败'
-            });
-          });
-          callback();
+        switch (this.register.certificate_value) {
+        case 0:
+          if (/^[1-9]\d{5}(18|19|([23]\d))\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$|^[1-9]\d{5}\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{2}$/.test(value) === false) {
+            callback(new Error());
+          } else {
+            callback(checkIdentify());
+            // TODO
+          }
+          break;
         }
+      };
+      let checkIdentify = (rule, value, callback) => {
+        this.$http.get('http://localhost:8081/patient/queryIdentify?identifyNumber=' + this.register.cardNumber).then(response => {
+          if (response.bodyText !== '') {
+            this.register.patientName = response.bodyText;
+          } else {
+            this.register.patientName = '';
+            // this.dialogVisible = true;
+          }
+        }, response => {
+          console.log('数据请求失败');
+        });
       };
       return {
         register: {
@@ -96,28 +101,31 @@
           expense: '',
           visit_date: ''
         },
-        departmentvalue: '',
-        doctorvalue: '',
+        dialogVisible: false,
+        certificateValue: '',
+        departmentValue: '',
+        doctorValue: '',
+        identifyType: [],
         department: [],
         doctor: [],
         rules: {
           patientName: [
-            { required: true, message: '请输入患者姓名', trigger: 'blur' }
+            {required: true, message: '请输入身份信息', trigger: 'change'}
           ],
-          certificate_value: [
-            { required: true, message: '请选择办理凭证', trigger: 'change' }
+          certificateValue: [
+            {required: true, message: '请选择办理凭证', trigger: 'change'}
           ],
           cardNumber: [
-            {required: true, validator: checkCertificate, trigger: 'blur'}
+            {required: true, message: '未识别身份信息', validator: checkCertificate, trigger: 'blur'}
           ],
-          departmentvalue: [
-            { required: true, message: '请至少选择一个科室', trigger: 'change' }
+          departmentValue: [
+            {required: true, message: '请至少选择一个科室', trigger: 'change'}
           ],
-          doctorvalue: [
-            { required: true, message: '请选择医生', trigger: 'change' }
+          doctorValue: [
+            {required: true, message: '请选择医生', trigger: 'change'}
           ],
           visit_time: [
-            { required: true, message: '请填写活动形式', trigger: 'blur' }
+            {required: true, message: '请填写活动形式', trigger: 'blur'}
           ]
         }
       };
@@ -129,13 +137,17 @@
       this.$http.get('http://localhost:8081/dept/all').then(response => {
         this.department = response.data;
       }, response => {
-        this.$notify.error({
-          message: '数据请求失败'
-        });
+        console.log('科室获取失败');
+      });
+      this.$http.get('http://localhost:8081/patient/queryIdentifyType').then(response => {
+        this.identifyType = response.data;
+      }, response => {
+        console.log('办理凭证获取失败');
       });
     },
     methods: {
       submitForm (formName) {
+        console.log(JSON.stringify(this.register));
         this.$refs.registerForm.validate(valid => {
           if (valid) {
             this.$notify.success({
@@ -169,14 +181,17 @@
       },
       compute_registerNumber () {
         this.$http.get('http://localhost:8081/patient/getLastSerialNumber');
+      },
+      toRegistry () {
+        this.$router.push({path: 'addPatient'});
       }
     },
     watch: {
-      departmentvalue: function () {
-        this.register.department_value = this.departmentvalue;
-        this.$http.get('http://localhost:8081/doctor/findBy?id=' + this.departmentvalue).then(response => {
+      departmentValue: function () {
+        this.register.department_value = this.departmentValue;
+        this.$http.get('http://localhost:8081/doctor/findBy?id=' + this.departmentValue).then(response => {
           this.doctor = [];
-          this.register.doctor_value = '';
+          this.doctorValue = '';
           for (let i = 0; i < response.data.length; i++) {
             this.doctor.push(response.data[i]);
           }
@@ -186,13 +201,16 @@
           });
         });
       },
-      doctorvalue: function () {
-        this.register.doctor_value = this.doctorvalue;
+      doctorValue: function () {
+        this.register.doctor_value = this.doctorValue;
         for (let i = 0; i < this.doctor.length; i++) {
           if (this.register.doctor_value === this.doctor[i].name) {
             this.register.expense = this.doctor[i].expense;
           }
         }
+      },
+      certificateValue: function () {
+        this.register.certificate_value = this.certificateValue;
       }
     }
   };
@@ -206,6 +224,7 @@
       padding-left: 20px
       .register-name
         width: 315px
+
   .register .el-input, .register .el-input__inner
     width: 210px
 </style>
