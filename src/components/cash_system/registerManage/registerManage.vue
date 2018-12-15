735cf843645d5264665fe738ca7da558be2e3bc4
zhouzy <saint.sola@yahoo.com>
2018-12-15 12:51:36 +0800

    add query patient model

--- src/components/cash_system/registerManage/registerManage.vue
+++ src/components/cash_system/registerManage/registerManage.vue
@@ -6,42 +6,39 @@
       <el-input placeholder="患者姓名" v-model="searchName"></el-input>
       <el-button type="primary" icon="search" @click="search">搜索</el-button>
       <el-button type="primary" icon="view" @click="showAll">显示全部</el-button>
-
       <el-button type="primary" icon="document" @click="handleDownload">导出</el-button>
     </div>
     <!-- 患者挂号信息列表 -->
-    <el-table :data="patients" border style="width: 100%; text-align:left">
-      <el-table-column prop="number" label="挂号序号"></el-table-column>
-      <el-table-column prop="name" label="患者姓名"></el-table-column>
-      <el-table-column prop="" label="挂号凭证"></el-table-column>
-      <el-table-column prop="" label="凭证号"></el-table-column>
-      <el-table-column prop="disease" label="科室"></el-table-column>
-      <el-table-column prop="doctor" label="就诊医生"></el-table-column>
-      <el-table-column prop="date" label="就诊时间"></el-table-column>
-      <el-table-column prop="" label="候诊号"></el-table-column>
-      <el-table-column prop="" label="挂号费"></el-table-column>
-      <el-table-column label="操作" width="180">
+    <el-table :data="patients" style="width: 80%; margin-left: 10%">
+      <el-table-column  width="80px" prop="序号" label="序号"></el-table-column>
+      <el-table-column  width="80px" prop="患者姓名" label="患者姓名"></el-table-column>
+      <el-table-column  width="80px" prop="挂号凭证" label="挂号凭证"></el-table-column>
+      <el-table-column  width="80px" prop="科室" label="科室"></el-table-column>
+      <el-table-column  width="80px" prop="就诊医生" label="就诊医生"></el-table-column>
+      <el-table-column  width="120px" prop="就诊日期" label="就诊日期"></el-table-column>
+      <el-table-column  width="120px" prop="就诊时间" label="就诊时间"></el-table-column>
+      <el-table-column  width="225px" prop="候诊号" label="候诊号"></el-table-column>
+      <el-table-column  width="80px" prop="挂号费" label="挂号费"></el-table-column>
+      <el-table-column  width="250px" label="操作">
         <template slot-scope="scope">
           <el-button type="primary" class="btn" @click="checkDetail(scope.$index)">查看</el-button>
-          <el-button type="danger" class="btn" @click="deleteDetail(scope.$index,patients)">删除</el-button>
+          <el-button class="btn" @click="checkDetail(scope.$index, patients)">缴费</el-button>
+          <el-button type="danger" class="btn" @click="deleteDetail(scope.$index, patients)">取消</el-button>
         </template>
       </el-table-column>
     </el-table>
     <!-- 点击查看患者挂号详细信息 -->
     <el-dialog title="患者挂号信息" :visible.sync="dialogVisible">
       <ul class="patients-detail">
-        <li><h5>挂号序号：</h5><span>{{patientsdetails.number}}</span></li>
-        <li><h5>患者姓名:</h5><span>{{patientsdetails.name}}</span></li>
-        <li><h5>挂号凭证:</h5><span></span></li>
-        <li><h5>凭证号：</h5><span></span></li>
-        <li><h5>性别:</h5><span>{{patientsdetails.sex}}</span></li>
-        <li><h5>年龄:</h5><span>{{patientsdetails.age}}</span></li>
-        <li><h5>家庭地址:</h5><span>{{patientsdetails.position}}</span></li>
-        <li><h5>科室:</h5><span>{{patientsdetails.disease}}</span></li>
-        <li><h5>就诊医生:</h5><span>{{patientsdetails.doctor}}</span></li>
-        <li><h5>就诊时间:</h5><span>{{patientsdetails.date}}</span></li>
-        <li><h5>候诊号:</h5><span></span></li>
-        <li><h5>挂号费:</h5><span></span></li>
+        <li><h5>序号：</h5><span>{{patientsdetails.序号}}</span></li>
+        <li><h5>患者姓名:</h5><span>{{patientsdetails.患者姓名}}</span></li>
+        <li><h5>挂号凭证:</h5><span>{{patientsdetails.挂号凭证}}</span></li>
+        <li><h5>科室:</h5><span>{{patientsdetails.科室}}</span></li>
+        <li><h5>就诊医生:</h5><span>{{patientsdetails.就诊医生}}</span></li>
+        <li><h5>就诊日期:</h5><span>{{patientsdetails.就诊日期}}</span></li>
+        <li><h5>就诊时间:</h5><span>{{patientsdetails.就诊时间}}</span></li>
+        <li><h5>候诊号:</h5><span>{{patientsdetails.候诊号}}</span></li>
+        <li><h5>挂号费:</h5><span>{{patientsdetails.挂号费}}</span></li>
       </ul>
     </el-dialog>
 
@@ -49,7 +46,6 @@
 </template>
 
 <script type="text/ecmascript-6">
-  import {api} from '../../../global/api.js';
   export default {
     data () {
       return {
@@ -62,14 +58,9 @@
       };
     },
     mounted () {
-      let registerThis = this;
-
-      registerThis.$http.get(api.patientList).then((response) => {
-        // 测试语句，测试是否能获取response
-        console.log(registerThis.patients, response);
-        // 把json接口获取的数据赋给当前对象
-        registerThis.patients = response.data.tableData;
-        registerThis.intendedpatients = response.data.tableData;
+      this.$http.get('http://localhost:8081/patient/queryRegisterList').then(response => {
+        this.intendedpatients = response.data;
+        this.patients = this.intendedpatients;
       }, response => {
         // error callback
         this.$message({
@@ -79,49 +70,32 @@
       });
     },
     methods: {
-      // 搜索
       search () {
-        let search = this;
-        search.$http.get(api.patientList, {params: {number: search.searchNumber, name: search.searchName}}).then((response) => {
-          // 把数组置空，以便存放搜索到的符合条件的数据
-          search.patients = [];
-          // 遍历后台数据是否有和传入的参数相同的，有的话就找出来push进prescriptionC[]
-          for (let i = 0; i < response.data.tableData.length; i++) {
-            // 两个条件都有输入值的时候，判断是否有同时符合这两个条件的数据
-            if (search.searchNumber && search.searchName) {
-              if (response.data.tableData[i].number === search.searchNumber && response.data.tableData[i].name === search.searchName) {
-                search.patients.push(response.data.tableData[i]);
-              }
-            } else {
-              if (search.searchNumber && response.data.tableData[i].number === search.searchNumber) {
-                // 只有第一个输入值的时候
-                search.patients.push(response.data.tableData[i]);
-              } else if (search.searchName && response.data.tableData[i].name === search.searchName) {
-                // 只有第二个输入值的时候
-                search.patients.push(response.data.tableData[i]);
-              }
+        this.patients = [];
+        for (let i = 0; i < this.intendedpatients.length; i++) {
+          if (this.searchNumber && this.searchName) {
+            if (this.intendedpatients[i].number === this.searchNumber && this.intendedpatients[i].患者姓名 === this.searchName) {
+              this.patients.push(this.intendedpatients[i]);
+            }
+          } else {
+            if (this.searchNumber && this.intendedpatients[i].number === this.searchNumber) {
+              this.patients.push(this.intendedpatients[i]);
+            } else if (this.searchName && this.intendedpatients[i].患者姓名 === this.searchName) {
+              this.patients.push(this.intendedpatients[i]);
             }
           }
-        }, (response) => {
-          // error callback
-          search.$notify.error({
-            message: '数据请求失败'
-          });
-        });
+        }
       },
-      // 显示全部数据
       showAll () {
         this.patients = this.intendedpatients;
         this.searchNumber = '';
         this.searchNamer = '';
       },
-      // 确认是否要关闭dialog
       handleClose (done) {
         this.$confirm('确认关闭？').then(_ => {
           done();
         }).catch(_ => {});
       },
-      // 查看患者信息详情
       checkDetail (index) {
         this.dialogVisible = true;
         this.patientsdetails = this.patients[index];
@@ -129,16 +103,13 @@
       deleteDetail (index, rows) {
         rows.splice(index, 1);
       },
-      // 把数据表格导出到excel表
       handleDownload () {
-        var vm = this;
-
         require.ensure([], () => {
           const { export_json_to_excel } = require('vendor/Export2Excel');
-          const tHeader = ['挂号序号', '患者姓名', '挂号凭证', '凭证号', '科室', '就诊医生', '就诊时间', '候诊号', '挂号费'];
-          const filterVal = ['number', 'name', '', '', 'disease', 'doctor', 'date', '', ''];
-          const patients = vm.patients;
-          const data = vm.formatJson(filterVal, patients);
+          const tHeader = ['序号', '患者姓名', '挂号凭证', '科室', '就诊医生', '就诊日期', '就诊时间', '候诊号', '挂号费'];
+          const filterVal = ['序号', '患者姓名', '挂号凭证', '就诊医生', '就诊日期', '就诊时间', 'date', '候诊号', '挂号费'];
+          const patients = this.patients;
+          const data = this.formatJson(filterVal, patients);
           export_json_to_excel(tHeader, data, '患者挂号信息列表excel');
         });
       },
@@ -153,11 +124,8 @@
   .registerManage .filter-container
       padding-bottom: 30px
 
-    .el-input
+  .el-input
       width: 200px
-
-    .el-table-column
-      font-weight: bold
 
     .patients-detail
       h5
@@ -168,4 +136,5 @@
 
     .btn
         display: inline-block
+
 </style>
