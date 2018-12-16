d43aad71b5a2937982a0a927134a723519aaeeb7
johannste <saint.sola@yahoo.com>
2018-12-16 11:15:43 +0800

    optimized registerManage

--- src/components/cash_system/registerManage/registerManage.vue
+++ src/components/cash_system/registerManage/registerManage.vue
@@ -1,33 +1,29 @@
 <template>
   <div class="registerManage">
-    <!-- 搜索条件 -->
     <div class="filter-container">
-      <el-input placeholder="处方编号" v-model="searchNumber"></el-input>
+      <!--TODO 先不做-->
+      <!--<el-input placeholder="处方编号" v-model="searchNumber"></el-input>-->
       <el-input placeholder="患者姓名" v-model="searchName"></el-input>
       <el-button type="primary" icon="search" @click="search">搜索</el-button>
       <el-button type="primary" icon="view" @click="showAll">显示全部</el-button>
       <el-button type="primary" icon="document" @click="handleDownload">导出</el-button>
     </div>
-    <!-- 患者挂号信息列表 -->
-    <el-table :data="patients" style="width: 80%; margin-left: 10%">
-      <el-table-column  width="80px" prop="序号" label="序号"></el-table-column>
-      <el-table-column  width="80px" prop="患者姓名" label="患者姓名"></el-table-column>
-      <el-table-column  width="80px" prop="挂号凭证" label="挂号凭证"></el-table-column>
-      <el-table-column  width="80px" prop="科室" label="科室"></el-table-column>
-      <el-table-column  width="80px" prop="就诊医生" label="就诊医生"></el-table-column>
-      <el-table-column  width="120px" prop="就诊日期" label="就诊日期"></el-table-column>
-      <el-table-column  width="120px" prop="就诊时间" label="就诊时间"></el-table-column>
-      <el-table-column  width="225px" prop="候诊号" label="候诊号"></el-table-column>
-      <el-table-column  width="80px" prop="挂号费" label="挂号费"></el-table-column>
-      <el-table-column  width="250px" label="操作">
+    <el-table :data="patients" style="width: 95%; margin-left: 2%">
+      <el-table-column width="100px" prop="序号" label="序号"></el-table-column>
+      <el-table-column width="100px" prop="患者姓名" label="患者姓名"></el-table-column>
+      <el-table-column width="100px" prop="科室" label="科室"></el-table-column>
+      <el-table-column width="120px" prop="就诊日期" label="就诊日期"></el-table-column>
+      <el-table-column width="120px" prop="就诊时间" label="就诊时间"></el-table-column>
+      <el-table-column width="215px" prop="候诊号" label="候诊号"></el-table-column>
+      <el-table-column width="100px" prop="挂号费" label="挂号费"></el-table-column>
+      <el-table-column width="250px" label="操作">
         <template slot-scope="scope">
-          <el-button type="primary" class="btn" @click="checkDetail(scope.$index)">查看</el-button>
-          <el-button class="btn" @click="checkDetail(scope.$index, patients)">缴费</el-button>
-          <el-button type="danger" class="btn" @click="deleteDetail(scope.$index, patients)">取消</el-button>
+          <el-button class="btn" @click="checkDetail(scope.$index)">查看</el-button>
+          <el-button v-if="patients[scope.$index].缴费状态==='未缴费'" type="primary" class="btn" @click="updatePay(scope.$index, patients)">缴费</el-button>
+          <el-button v-if="patients[scope.$index].挂号状态!=='有效'" type="danger" class="btn" @click="deleteDetail(scope.$index, patients)">取消</el-button>
         </template>
       </el-table-column>
     </el-table>
-    <!-- 点击查看患者挂号详细信息 -->
     <el-dialog title="患者挂号信息" :visible.sync="dialogVisible">
       <ul class="patients-detail">
         <li><h5>序号：</h5><span>{{patientsdetails.序号}}</span></li>
@@ -39,6 +35,8 @@
         <li><h5>就诊时间:</h5><span>{{patientsdetails.就诊时间}}</span></li>
         <li><h5>候诊号:</h5><span>{{patientsdetails.候诊号}}</span></li>
         <li><h5>挂号费:</h5><span>{{patientsdetails.挂号费}}</span></li>
+        <li><h5>缴费状态:</h5><span>{{patientsdetails.缴费状态}}</span></li>
+        <li><h5>挂号状态:</h5><span>{{patientsdetails.挂号状态}}</span></li>
       </ul>
     </el-dialog>
 
@@ -62,11 +60,7 @@
         this.intendedpatients = response.data;
         this.patients = this.intendedpatients;
       }, response => {
-        // error callback
-        this.$message({
-          message: '数据请求失败',
-          type: 'error'
-        });
+        console.error('数据请求失败');
       });
     },
     methods: {
@@ -100,14 +94,36 @@
         this.dialogVisible = true;
         this.patientsdetails = this.patients[index];
       },
+      updatePay (index, rows) {
+        this.$http.get('http://localhost:8081/patient/updateRegisterPayment?id=' + rows[index].序号).then(response => {
+          this.$message.success({
+            message: '缴费成功'
+          });
+          this.intendedpatients[index].缴费状态 = '已缴费';
+          this.intendedpatients[index].挂号状态 = '有效';
+        }, response => {
+          this.$message.error({
+            message: '缴费失败'
+          });
+        });
+      },
       deleteDetail (index, rows) {
-        rows.splice(index, 1);
+        this.$http.get('http://localhost:8081/patient/cancelRegisterStatus?id=' + rows[index].序号).then(response => {
+          this.$message.success({
+            message: '取消成功'
+          });
+          rows.splice(index, 1);
+        }, response => {
+          this.$message.error({
+            message: '取消失败'
+          });
+        });
       },
       handleDownload () {
         require.ensure([], () => {
           const { export_json_to_excel } = require('vendor/Export2Excel');
-          const tHeader = ['序号', '患者姓名', '挂号凭证', '科室', '就诊医生', '就诊日期', '就诊时间', '候诊号', '挂号费'];
-          const filterVal = ['序号', '患者姓名', '挂号凭证', '就诊医生', '就诊日期', '就诊时间', 'date', '候诊号', '挂号费'];
+          const tHeader = ['序号', '患者姓名', '挂号凭证', '科室', '就诊医生', '就诊日期', '就诊时间', '候诊号', '挂号费', '挂号状态', '缴费状态'];
+          const filterVal = ['序号', '患者姓名', '挂号凭证', '就诊医生', '就诊日期', '就诊时间', 'date', '候诊号', '挂号费', '挂号状态', '缴费状态'];
           const patients = this.patients;
           const data = this.formatJson(filterVal, patients);
           export_json_to_excel(tHeader, data, '患者挂号信息列表excel');
@@ -126,6 +142,7 @@
 
   .el-input
       width: 200px
+      margin: 0 5% 0 2%
 
     .patients-detail
       h5
