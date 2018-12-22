dd0134ea9a1abd15ec54ab4d47b35a79ad8e2b91
johannste <saint.sola@yahoo.com>
2018-12-23 00:31:24 +0800

    add el-pagination

--- src/components/cash_system/registerManage/registerManage.vue
+++ src/components/cash_system/registerManage/registerManage.vue
@@ -4,16 +4,16 @@
       <!--TODO 先不做-->
       <!--<el-input placeholder="处方编号" v-model="searchNumber"></el-input>-->
       <el-input placeholder="患者姓名" v-model="searchName"></el-input>
-      <el-button type="primary" icon="search" @click="search">搜索</el-button>
+      <el-button type="primary" icon="search" @click="searchPatient">搜索</el-button>
       <el-button type="primary" icon="view" @click="showAll">显示全部</el-button>
       <el-button type="primary" icon="document" @click="handleDownload">导出</el-button>
     </div>
-    <el-table :data="patients" style="width: 95%; margin-left: 2%">
+    <el-table :data="patients" class="register-table">
       <el-table-column sortable width="100px" prop="序号" label="序号"></el-table-column>
       <el-table-column width="100px" prop="患者姓名" label="患者姓名"></el-table-column>
       <el-table-column width="100px" prop="科室" label="科室"></el-table-column>
-      <el-table-column sortable width="120px" prop="就诊日期" label="就诊日期"></el-table-column>
-      <el-table-column sortable width="120px" prop="就诊时间" label="就诊时间"></el-table-column>
+      <el-table-column sortable width="127px" prop="就诊日期" label="就诊日期"></el-table-column>
+      <el-table-column sortable width="127px" prop="就诊时间" label="就诊时间"></el-table-column>
       <el-table-column sortable width="215px" prop="候诊号" label="候诊号"></el-table-column>
       <el-table-column width="100px" prop="挂号费" label="挂号费"></el-table-column>
       <el-table-column width="250px" label="操作">
@@ -39,7 +39,11 @@
         <li><h5>挂号状态:</h5><span>{{patientsDetails.挂号状态}}</span></li>
       </ul>
     </el-dialog>
-
+    <div class="block">
+      <el-pagination @size-change="handleSizeChange" @current-change="queryRegisterListByPage" :current-page.sync="currentPage" :page-sizes="[3, 10, 15, 20]" :page-size="pageSize" layout="sizes, prev, pager, next" :total="totalCount">
+      </el-pagination>
+    </div>
+  </div>
   </div>
 </template>
 
@@ -51,32 +55,51 @@
         intendedPatients: [],
         dialogVisible: false,
         patientsDetails: '',
-        searchNumber: '',
-        searchName: ''
+        patientsExportDetails: '',
+        searchName: '',
+        currentPage: 1,
+        totalCount: 0,
+        pageSize: 3
       };
     },
     mounted () {
-      this.$axios.get('http://localhost:8081/patient/queryRegisterList').then(response => {
-        this.intendedPatients = response.data;
-        this.patients = this.intendedPatients;
-      }).catch(error => {
-        console.error(error);
-      });
+      this.showAll();
     },
     methods: {
-      search () {
+      queryRegisterListByPage () {
+        this.$axios.get('http://localhost:8081/patient/queryRegisterListByPage?begin=' + (this.currentPage - 1) * this.pageSize + '&end=' + this.currentPage * this.pageSize).then(response => {
+          this.patients = response.data;
+        }).catch(error => {
+          console.error(error);
+        });
+      },
+      handleSizeChange (val) {
+        this.pageSize = val;
+        this.queryRegisterListByPage();
+      },
+      searchPatient () {
         this.patients = [];
         for (let i = 0; i < this.intendedPatients.length; i++) {
-          if (this.searchNumber && this.intendedPatients[i].number === this.searchNumber) {
-            this.patients.push(this.intendedPatients[i]);
-          } else if (this.searchName && this.intendedPatients[i].患者姓名 === this.searchName) {
+          if (this.intendedPatients[i].患者姓名 === this.searchName) {
             this.patients.push(this.intendedPatients[i]);
           }
         }
+        // TODO 子页面搜索
+        this.patientsExportDetails = this.patients;
       },
       showAll () {
-        this.patients = this.intendedPatients;
-        this.searchNumber = '';
+        this.$axios.get('http://localhost:8081/patient/queryRegisterList').then(response => {
+          this.intendedPatients = response.data;
+          this.patientsExportDetails = this.intendedPatients;
+          this.totalCount = response.data.length;
+        }).catch(error => {
+          console.error(error);
+        });
+        this.$axios.get('http://localhost:8081/patient/queryRegisterListByPage?begin=' + (this.currentPage - 1) * this.pageSize + '&end=' + this.currentPage * this.pageSize).then(response => {
+          this.patients = response.data;
+        }).catch(error => {
+          console.error(error);
+        });
       },
       handleClose (done) {
         this.$confirm('确认关闭？').then(_ => {
@@ -115,8 +138,7 @@
           const { export_json_to_excel } = require('vendor/Export2Excel');
           const tHeader = ['序号', '患者姓名', '挂号凭证', '科室', '就诊医生', '就诊日期', '就诊时间', '候诊号', '挂号费', '挂号状态', '缴费状态'];
           const filterVal = ['序号', '患者姓名', '挂号凭证', '就诊医生', '就诊日期', '就诊时间', 'date', '候诊号', '挂号费', '挂号状态', '缴费状态'];
-          const patients = this.patients;
-          const data = this.formatJson(filterVal, patients);
+          const data = this.formatJson(filterVal, this.patientsExportDetails);
           export_json_to_excel(tHeader, data, '患者挂号信息列表excel');
         });
       },
@@ -128,8 +150,13 @@
 </script>
 
 <style lang="stylus-loader" rel="stylesheet/stylus" type="text/stylus">
-  .registerManage .filter-container
+  .filter-container
       padding-bottom: 30px
+
+  .register-table
+    width: 1120px
+    margin-left: auto
+    margin-right: auto
 
   .el-input
       width: 200px
@@ -143,6 +170,9 @@
         font-weight: bold
 
     .btn
-        display: inline-block
+      display: inline-block
 
+  .block
+    margin-top: 28px
+    margin-left: 1.5%
 </style>
